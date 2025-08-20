5447cc75beaf450354698fc31d793ceee97c22b0
Fer Lazaro <skrilroses@gmail.com>
2025-08-20 11:16:47 -0600

    Prueba de app JS

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let numeroAmigos = [];
+
+function agregarAmigo(){
+    const amigo = document.getElementById('amigo').value;
+    numeroAmigos.push(amigo);
+    console.log(numeroAmigos);
+}
