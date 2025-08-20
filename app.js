da2720fbe69143c7e95ec29caa6581b84c97ec77
Fer Lazaro <skrilroses@gmail.com>
2025-08-20 12:17:19 -0600

    Funciones de agregar amigo, listas y sorteo actualizadas

--- app.js
+++ app.js
@@ -2,7 +2,57 @@
 let numeroAmigos = [];
 
 function agregarAmigo(){
+    // Entrada del usuario
     const amigo = document.getElementById('amigo').value;
-    numeroAmigos.push(amigo);
+    
+    if (amigo === ''){
+        // Detecta elementos vacíos
+        alert('Por favor, ingresa un nombre para agregarlo a la lista.');
+    } else {
+        //Agrega nombre al final
+        numeroAmigos.push(amigo);
+        // Limpia la caja
+        const limpiarCaja = document.getElementById('amigo');
+        limpiarCaja.value = '';
+    }
+    // Actualizamos la lista
+    actualizarListaAmigos();
+    // Verificar tamaño de arreglo y contenido
     console.log(numeroAmigos);
+}
+
+function actualizarListaAmigos(){
+    // extrae el id de la lista <ul>
+    const listAmigos = document.getElementById('listaAmigos');
+    // limpia la lista 
+    listAmigos.innerHTML = '';
+
+    numeroAmigos.forEach(amigo => {
+        // crea un <li>
+        const li = document.createElement('li');
+        // asignar texto del amigo actual
+        li.textContent = amigo;
+        // agregar <li> como hijo de <ul> en el HTML
+        listAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    // Obtiene el <ul> donde se mostrará el resultado
+    const resultadoElemento = document.getElementById('resultado');
+
+    // valida que el array de los amigos no esté vacío
+    if (numeroAmigos.length === 0) {
+        alert("No hay ningún amigo :(");
+        return;
+    }
+
+    // Genera un índice aleatorio que no supere el rango del array
+    const i = Math.floor(Math.random() * numeroAmigos.length);
+    
+    // Obtiene un amigo con ese índice
+    const amigoSorteado = numeroAmigos[i];
+
+    // Muestra el resultado
+    resultadoElemento.innerHTML = `El amigo sorteado es: <strong>${amigoSorteado}</strong>`;
 }
